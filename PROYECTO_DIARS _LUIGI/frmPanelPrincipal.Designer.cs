44f91debc9fc03dd8a55e6ede81ec84275f9af2e
LUIGI ENRIQUE MENDOZA HERRERA <mendozaherreraluigi@gmail.com>
2024-09-20 21:20:35 -0500

    añadi algunos elemento en el panel principal

--- PROYECTO_DIARS _LUIGI/frmPanelPrincipal.Designer.cs
+++ PROYECTO_DIARS _LUIGI/frmPanelPrincipal.Designer.cs
@@ -28,19 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tlpPrincipal = new System.Windows.Forms.TableLayoutPanel();
+            this.tabControlPanel = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
             this.btnSalir = new System.Windows.Forms.Button();
+            this.btnPerfil = new System.Windows.Forms.Button();
+            this.panelFormAbierto = new System.Windows.Forms.Panel();
+            this.tlpPrincipal.SuspendLayout();
+            this.tabControlPanel.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tlpPrincipal
+            // 
+            this.tlpPrincipal.ColumnCount = 1;
+            this.tlpPrincipal.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpPrincipal.Controls.Add(this.tabControlPanel, 0, 0);
+            this.tlpPrincipal.Controls.Add(this.panelFormAbierto, 0, 1);
+            this.tlpPrincipal.Location = new System.Drawing.Point(12, 12);
+            this.tlpPrincipal.Name = "tlpPrincipal";
+            this.tlpPrincipal.RowCount = 2;
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tlpPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpPrincipal.Size = new System.Drawing.Size(1131, 648);
+            this.tlpPrincipal.TabIndex = 8;
+            // 
+            // tabControlPanel
+            // 
+            this.tabControlPanel.Controls.Add(this.tabPage2);
+            this.tabControlPanel.Controls.Add(this.tabPage1);
+            this.tabControlPanel.Controls.Add(this.tabPage3);
+            this.tabControlPanel.Location = new System.Drawing.Point(3, 3);
+            this.tabControlPanel.Name = "tabControlPanel";
+            this.tabControlPanel.SelectedIndex = 0;
+            this.tabControlPanel.Size = new System.Drawing.Size(1125, 114);
+            this.tabControlPanel.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1117, 88);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Mantenimiento";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnPerfil);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1117, 88);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Perfil";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1117, 88);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Doc SUNAT";
+            this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // btnSalir
             // 
             this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(1821, 35);
+            this.btnSalir.Location = new System.Drawing.Point(1113, 12);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(30, 30);
-            this.btnSalir.TabIndex = 7;
+            this.btnSalir.TabIndex = 9;
             this.btnSalir.Text = "X";
             this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnPerfil
+            // 
+            this.btnPerfil.Location = new System.Drawing.Point(6, 6);
+            this.btnPerfil.Name = "btnPerfil";
+            this.btnPerfil.Size = new System.Drawing.Size(75, 75);
+            this.btnPerfil.TabIndex = 0;
+            this.btnPerfil.Text = "Perfil";
+            this.btnPerfil.UseVisualStyleBackColor = true;
+            // 
+            // panelFormAbierto
+            // 
+            this.panelFormAbierto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelFormAbierto.Location = new System.Drawing.Point(3, 123);
+            this.panelFormAbierto.Name = "panelFormAbierto";
+            this.panelFormAbierto.Size = new System.Drawing.Size(1125, 522);
+            this.panelFormAbierto.TabIndex = 1;
             // 
             // frmPanelPrincipal
             // 
@@ -48,15 +131,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1900, 1080);
             this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.tlpPrincipal);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmPanelPrincipal";
             this.Text = "frmPanelPrincipal";
+            this.tlpPrincipal.ResumeLayout(false);
+            this.tabControlPanel.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TableLayoutPanel tlpPrincipal;
+        private System.Windows.Forms.TabControl tabControlPanel;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnPerfil;
+        private System.Windows.Forms.Panel panelFormAbierto;
     }
 }
